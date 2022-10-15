8a757042e1c08b6dfd49e7149ed3311a77837d12
MohammadHaque27 <mhaque1@usf.edu>
2022-10-15 15:51:27 -0400

    updated

---
+++ FIFO.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstdlib>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+//Function for FIFO Algorithm 
+void FIFO(int array, int number_of_frames){
+        
+    
+}
+
+
