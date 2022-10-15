cb99776f9588fe1c67a78ccab7a8cdc2eaff4cdf
MohammadHaque27 <mhaque1@usf.edu>
2022-10-15 15:36:53 -0400

    initialized

---
+++ Project2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstdlib>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+//Function for FIFO Algorithm 
+
+
+
+//Function for LRU Algorithm 
+void FIFO
+
+//Function for Segmented FIFO Algorithm 
