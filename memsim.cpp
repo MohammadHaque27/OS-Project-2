f183ec4537724b3a3e1387ef9b0336f534b99994
Ja-Hansen <jahansen1@usf.edu>
2022-10-16 14:10:36 -0400

    idk what this change is

--- memsim.cpp
+++ memsim.cpp
@@ -1,10 +1,15 @@
-#include <iostream>
+//#include <iostream>
+#include <stdio>
 #include "dataStructures.hpp"
 #include "policies.hpp"
 
 
 int main(int argc, char* argv[]) //memsim <tracefile> <nframes> <lru|fifo|vms> <p> <debug|quiet> 
 {
+    
+    
+    
+    
     unsigned addr; 
     char rw; //had .. after this 
     fscanf(file,"%x %c",&addr,&rw);
