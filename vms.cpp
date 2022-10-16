c5fb4669f8dad0cf0f5f2428b3e56da405189376
Ja-Hansen <jahansen1@usf.edu>
2022-10-16 17:05:21 -0400

    updates

--- vms.cpp
+++ vms.cpp
@@ -1,1 +1,28 @@
+#include "policies.hpp"
+#include "dataStructures.hpp"
 
+
+void VMS()
+{
+//use a different class for each page table object based on what the replacement algorithm needs
+    CircularArray pageTable(argv[1]); //argv[1] = <nframes>
+    
+    FILE * tracefile;
+    tracefile = fopen(argv[0], "r"); //argv[0] = <tracefile>
+    
+    unsigned addr; 
+    char rw;
+    while (fscanf(tracefile,"%x %c",&addr,&rw) != 0)
+    {
+        addr = 0x(addr) / 4096; //Extract frame number by removing the 12 offset bits
+        //loop to compare addr to each page table entry
+    }
+
+
+
+
+
+    fclose(argv[0]); //argv[0] = <tracefile>
+    
+    return 0;
+}
