d8873d17cd46de3b1b6b1bb21e908fc26fbc8b6f
MohammadHaque27 <mhaque1@usf.edu>
2022-10-16 17:35:38 -0400

    Update

--- lru.cpp
+++ lru.cpp
@@ -10,7 +10,7 @@
 //Function for the LRU Algorithm
 void lru(int frame_num, std::vector<int> &array_object)
 {
-    /*
+    
     //use a different class for each page table object based on what the replacement algorithm needs
     CircularArray pageTable(nframes);
     
@@ -21,13 +21,9 @@ void lru(int frame_num, std::vector<int> &array_object)
     char rw;
     while (fscanf(tracefile,"%x %c",&addr,&rw) != 0)
     {
-        addr = 0x(addr) / 4096; //Extract frame number by removing the 12 offset bits
+        frameNum = addr / 4096; //Extract frame number by removing the 12 offset bits
         //loop to compare addr to each page table entry
     }
-    fclose(tracefile);
-    */
-    
-    
     
     
     // if the page is not in LRU and the LRU is full
@@ -42,3 +38,5 @@ void lru(int frame_num, std::vector<int> &array_object)
     array_object.push_back(temp);// adds the temp to the last postion of the array
     std:: << cout << array_object;
 }
+
+fclose(tracefile);
