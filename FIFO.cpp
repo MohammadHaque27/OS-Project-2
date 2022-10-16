f08df70ce99c7ee407cbc796292b072feee515d4
caitlina131 <caitlina131@gmail.com>
2022-10-16 17:35:35 -0400

    update

--- FIFO.cpp
+++ FIFO.cpp
@@ -7,39 +7,53 @@
 
 
 //Function for FIFO Algorithm 
-void FIFO(CircularArray &pageTable, unsigned frameNum){
-
-
-
-
+void FIFO(int nframes, int frames[7]){
+    //use a different class for each page table object based on what the replacement algorithm needs
+    CircularArray pageTable(nframes); //passed as argument
     
-    int offset = pageTable.getLoopOffset();
-    int size = pageTable.getSize();
-    int position = pageTable.calculateCircularIndex();
+    // FILE * tracefile;
+    // tracefile = fopen(traceName, "r"); //argv[0] = <tracefile>
+    
+    // unsigned addr; 
+    // char rw;
+    // while (fscanf(tracefile,"%x %c",&addr,&rw) != 0) //Loop: read each address from trace file
+    // {
+    //     addr = addr / 4096; //Extract frame number by removing the 12 offset bits
+    //     //loop to compare addr to each page table entry
+    // }
+
     // std::cout<< offset << " " << size << " " << position <<  " ";
     // pageTable.incrementLoopOffset();
     // std::cout<< pageTable.getLoopOffset() << " ";
-    int replacementIndex = pageTable.calculateCircularIndex();
+
     //std::cout << replacementIndex << frameNum.c_str() << std::endl;
-    std::cout << *pageTable.array << " ";
-    pageTable.array[replacementIndex] = frameNum;
+
+    //This is the replacement policy code if the page isn't found and it isn't empty
+        //std::cout << i << " ";
+    //     int replacementIndex = pageTable.calculateCircularIndex();
+    //     pageTable.array[replacementIndex] = addr;
+    //     pageTable.incrementLoopOffset();
+    
+    
 
 
-    std::cout<< "Successfully added\n";
-    for (int i = 0,=; i <= pageTable.getSize(); i++) {
-        std::cout<< pageTable.array[i] << ", ";
-    }
+    // std::cout<< "Successfully added\n";
+    // for (int i = 0; i < pageTable.getSize(); i++) {
+    //     std::cout<< pageTable.array[i] << ", ";
+    // }
     
 
     //std::cout << size << std::endl;
-    
+    // fclose(argv[0]); //argv[0] = <tracefile>
 }
 
 
 int main(){
-    CircularArray pageTable(4);
-    FIFO(pageTable, 0x1f16ff60);
-    
+    //int frames[7] = {0x0042bf60, 0x2f293f60, 0x1f16ff60, 0x0041f760, 0x37c1f760, 0x0041f840, 0x0041f860};
+    //FIFO(4, frames);
+    unsigned address = 0042bf60;
+    unsigned addr = address / 4096;
+    std::cout<<addr<<" ";
 
     return 0;
 }
