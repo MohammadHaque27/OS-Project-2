d6bf17710b4e168beb9a3a39219d4e36bca79d8b
MohammadHaque27 <mhaque1@usf.edu>
2022-10-16 10:00:17 -0400

    Updated LRU

--- lru.cpp
+++ lru.cpp
@@ -8,12 +8,12 @@
 
 
 //Function for the LRU Algorithm
-void lru(int frame_num, int *array_object)
+void lru(int frame_num, std::vector<int> &array_object)
 {
     // if the page is not in LRU and the LRU is full
     //pop the bottom of the array 
     //add the new page to the top
     int temp = frame_num;
-    *array_object.erase(*array_object.begin());
-    *array_object.push_back(temp);
+    array_object.erase(array_object.begin());
+    array_object.push_back(temp);
 }
