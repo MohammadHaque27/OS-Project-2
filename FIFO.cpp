afcd35beb5c811e65d4d838b0ff1ac5203c2620a
caitlina131 <caitlina131@gmail.com>
2022-10-15 21:35:55 -0400

    Updated

--- FIFO.cpp
+++ FIFO.cpp
@@ -3,15 +3,24 @@
 #include<cstdlib>
 #include <string>
 #include <vector>
-#include "dataStructures.hpp"
+#include "dataStructures.cpp"
 
 using namespace std;
 
 //Function for FIFO Algorithm 
-void FIFO(CircularArray* pageTable){
+void FIFO(CircularArray &pageTable){
     int offset = pageTable.getLoopOffset();
     int size = pageTable.getSize();
     int position = offset % size;
-
+    std::cout << size << std::endl;
     
+}
+
+
+int main(){
+    CircularArray pageTable(4);
+    FIFO(pageTable);
+
+
+    return 0;
 }
