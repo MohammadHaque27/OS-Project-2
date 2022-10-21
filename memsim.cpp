1a50dd143163c0d2eb83ea412416eba77031cbcb
caitlina131 <caitlina131@gmail.com>
2022-10-21 16:04:56 -0600

    Update memsim.cpp

--- memsim.cpp
+++ memsim.cpp
@@ -1,13 +1,62 @@
 //#include <iostream>
-#include <stdio>
-#include <string> //technically already included in a .hpp file
+#include <stdio.h>
+#include <iostream>
+#include <string.h> //technically already included in a .hpp file
 #include "dataStructures.hpp"
 #include "policies.hpp"
 
 
 int main(int argc, char* argv[]) //memsim <tracefile> <nframes> <lru|fifo|vms> <p> <debug|quiet> 
 {
-    
+    char *tracefile, *algorithm, *debugOrQuiet;
+    int nframes;
+    int p = 0;
+
+    if(argc<5) {
+        std::cout<<"You have too few arguments!"<<std::endl;
+    }
+
+    if(argc == 5) {
+        if (argv[3] == "lru" || argv[3] == "fifo"){
+            tracefile = argv[1];
+            nframes = std::stoi(argv[2]);
+            algorithm = argv[3];
+            debugOrQuiet = argv[4];
+        }
+        else{
+            std::cout << "vms requires the p parameter!" << std::endl;
+        }
+        
+    }
+
+    else if (argc == 6) {
+        if (argv[3] == "vms") {
+            tracefile = argv[1];
+            nframes = std::stoi(argv[2]);
+            algorithm = argv[3];
+            p = std::stoi(argv[4]);
+            debugOrQuiet = argv[5];
+        }
+        else {
+            std::cout << "fifo and lru do not require parameter p." << std::endl;
+        }
+    }
+
+    if(strcmp(algorithm, "fifo") == 0) {
+        fifo(nframes, tracefile, debugOrQuiet);
+    }
+
+    else if (strcmp(algorithm, "lru") == 0) {
+        //PLEASE CALL YOUR FUNCTION
+    }
+
+    else if (strcmp(algorithm, "vms") == 0) {
+        //PLEASE CALL YOUR FUNCTION
+    }
+
+    else {
+        std::cout << "You have entered an invalid algorithm" << std::endl;
+    }
     
     return 0;
 }
