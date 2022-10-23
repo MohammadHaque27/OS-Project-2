1e83345424145304697eb770e1237ba8acca97ef
Ja-Hansen <jahansen1@usf.edu>
2022-10-23 02:43:09 -0400

    u

--- policies.hpp
+++ policies.hpp
@@ -4,4 +4,4 @@
 
 void FIFO(int nframes, char* traceName, char *debugOrQuiet); //previously debugOrQuiet was char *mode
 void lru(const char* traceName , int frame_Num, std::string option);
-void VMS(const char* traceName, int nframes, int p, char *debugOrQuiet);
+void VMS(char* traceName, int nframes, int p, char *debugOrQuiet);
