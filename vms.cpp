72cb25e8bb011c93869e1eb474242780f31293de
Ja-Hansen <jahansen1@usf.edu>
2022-10-23 21:01:58 -0400

    indent

--- vms.cpp
+++ vms.cpp
@@ -13,167 +13,167 @@ void VMS(char* traceName, int nframes, int p, char *debugOrQuiet)
     }
     
     //conditional to just run fifo or lru (figure out percentages)
-    if (size1 == 0)
+    if (size1 <= 1)
     {
         lru(traceName, nframes, debugOrQuiet);
     }
-
-    if (size2 == 0)
+    else if (size2 <= 1)
     {
         FIFO(nframes, traceName, debugOrQuiet);
     }
-
-    CircularArray buffer1(size1);
-    std::vector<int>::iterator It;
-    std::vector<int> buffer2;
-    std::map<int, char> mem;
-    //buffer2.reserve(size2);
-    //int bufferHits = 0; //what do I need this for again?
-    int diskReads = 0;
-    int diskWrites = 0;
-    int count = 0;
-
-    FILE * tracefile;
-    tracefile = fopen(traceName, "r");
-    
-    unsigned addr; 
-    unsigned frameNum;
-    char rw;
-    //loop over each memory address in trace file
-    while (fscanf(tracefile,"%x %c",&addr,&rw) != EOF)
+    else
     {
-        count++;
-        frameNum = addr / 4096; //Extract frame number by removing the 12 offset bits
-        //loop to compare frameNum to each page table entry
-        for (int i = 0; i < size1; i++) 
+        CircularArray buffer1(size1);
+        std::vector<int>::iterator It;
+        std::vector<int> buffer2;
+        std::map<int, char> mem;
+        //buffer2.reserve(size2);
+        //int bufferHits = 0; //what do I need this for again?
+        int diskReads = 0;
+        int diskWrites = 0;
+        int count = 0;
+
+        FILE * tracefile;
+        tracefile = fopen(traceName, "r");
+        
+        unsigned addr; 
+        unsigned frameNum;
+        char rw;
+        //loop over each memory address in trace file
+        while (fscanf(tracefile,"%x %c",&addr,&rw) != EOF)
         {
-            if (frameNum == buffer1.array[i].first) //If entry is in FIFO table
+            count++;
+            frameNum = addr / 4096; //Extract frame number by removing the 12 offset bits
+            //loop to compare frameNum to each page table entry
+            for (int i = 0; i < size1; i++) 
             {
-                if(strcmp(debugOrQuiet, "debug") == 0){
-                    std::cout<< count <<": Buffer1 entry " << i << " is a match" << std::endl;
-                }
-                //bufferHits++;
-                if ((buffer1.array[i].second != 'W') && (rw == 'W'))
+                if (frameNum == buffer1.array[i].first) //If entry is in FIFO table
                 {
-                    buffer1.array[i].second = rw;
-                }
-                // if (rw == 'R')
-                // {
-                //     diskReads++;
-                // }
-                break;
-            }
-            else if (buffer1.array[i].first == 0)//If entry not in FIFO table but FIFO table entry is empty
-            {
-                if(strcmp(debugOrQuiet, "debug") == 0){
-                    std::cout<< count <<": Buffer1 entry " << i << " is empty, adding frame to the table" << std::endl;
-                }
-                buffer1.array[i].first = frameNum;
-                buffer1.array[i].second = rw;
-                if (rw == 'R')
-                {
-                    diskReads++;
                     if(strcmp(debugOrQuiet, "debug") == 0){
-                        std::cout<< count <<": Disk Read" << std::endl;
+                        std::cout<< count <<": Buffer1 entry " << i << " is a match" << std::endl;
                     }
+                    //bufferHits++;
+                    if ((buffer1.array[i].second != 'W') && (rw == 'W'))
+                    {
+                        buffer1.array[i].second = rw;
+                    }
+                    // if (rw == 'R')
+                    // {
+                    //     diskReads++;
+                    // }
+                    break;
                 }
-                break;
-            }
-            else if (i < (size1-1)) //Table not full
-            {
-                continue;
-            }
-            else //If entry is not in FIFO table and FIFO is full...
-            {
-                //...search through LRU table
-                if(strcmp(debugOrQuiet, "debug") == 0){
-                    std::cout<< count <<": Buffer1 is full with no matches, begin searching buffer2" << std::endl;
-                }
-                int code;
-                It = find(buffer2.begin(), buffer2.end(), frameNum); //Look for a frameNum match
-                if(It == buffer2.end()) //set switch code for frame not in LRU table
+                else if (buffer1.array[i].first == 0)//If entry not in FIFO table but FIFO table entry is empty
                 {
-                    code = 1;  
-                }
-                else  //set switch code for frame in LRU table
-                {
-                    code = 2;
-                } 
-                
-                switch(code) 
-                {
-                    case 1: //frame not in LRU table
-                        // diskReads++;
-                        // if(strcmp(debugOrQuiet, "debug") == 0){
-                        //         std::cout<< count <<": Disk Read" << std::endl;
-                        //     }
-                        if (rw == 'R')
-                        {
-                            diskReads++;
-                            if(strcmp(debugOrQuiet, "debug") == 0){
-                                std::cout<< count <<": Disk Read" << std::endl;
-                            }
+                    if(strcmp(debugOrQuiet, "debug") == 0){
+                        std::cout<< count <<": Buffer1 entry " << i << " is empty, adding frame to the table" << std::endl;
+                    }
+                    buffer1.array[i].first = frameNum;
+                    buffer1.array[i].second = rw;
+                    if (rw == 'R')
+                    {
+                        diskReads++;
+                        if(strcmp(debugOrQuiet, "debug") == 0){
+                            std::cout<< count <<": Disk Read" << std::endl;
                         }
-                        if(mem.size() < size2) //if LRU table not full
-                        {
-                            if(strcmp(debugOrQuiet, "debug") == 0){
-                                std::cout<< count <<": Buffer2 entry is empty, adding frame to the table" << std::endl;
+                    }
+                    break;
+                }
+                else if (i < (size1-1)) //Table not full
+                {
+                    continue;
+                }
+                else //If entry is not in FIFO table and FIFO is full...
+                {
+                    //...search through LRU table
+                    if(strcmp(debugOrQuiet, "debug") == 0){
+                        std::cout<< count <<": Buffer1 is full with no matches, begin searching buffer2" << std::endl;
+                    }
+                    int code;
+                    It = find(buffer2.begin(), buffer2.end(), frameNum); //Look for a frameNum match
+                    if(It == buffer2.end()) //set switch code for frame not in LRU table
+                    {
+                        code = 1;  
+                    }
+                    else  //set switch code for frame in LRU table
+                    {
+                        code = 2;
+                    } 
+                    
+                    switch(code) 
+                    {
+                        case 1: //frame not in LRU table
+                            // diskReads++;
+                            // if(strcmp(debugOrQuiet, "debug") == 0){
+                            //         std::cout<< count <<": Disk Read" << std::endl;
+                            //     }
+                            if (rw == 'R')
+                            {
+                                diskReads++;
+                                if(strcmp(debugOrQuiet, "debug") == 0){
+                                    std::cout<< count <<": Disk Read" << std::endl;
+                                }
                             }
-                            buffer2.push_back(frameNum);
-                            mem[frameNum] = rw;
-                        }
-                        else //LRU table is full
-                        {
+                            if(mem.size() < size2) //if LRU table not full
+                            {
+                                if(strcmp(debugOrQuiet, "debug") == 0){
+                                    std::cout<< count <<": Buffer2 entry is empty, adding frame to the table" << std::endl;
+                                }
+                                buffer2.push_back(frameNum);
+                                mem[frameNum] = rw;
+                            }
+                            else //LRU table is full
+                            {
+                                if(strcmp(debugOrQuiet, "debug") == 0){
+                                    std::cout<< count <<": Buffer2 is full with no matches, adding frame to buffer1 and shifting buffer1 replacement to buffer2" << std::endl;
+                                }
+                                int replacementIndex = buffer1.calculateCircularIndex();
+                                unsigned tempFrame = buffer1.array[replacementIndex].first;
+                                char tempRW = buffer1.array[replacementIndex].second;
+
+                                buffer1.array[replacementIndex].first = frameNum;
+                                buffer1.array[replacementIndex].second = rw;
+                                buffer1.incrementLoopOffset();
+                                
+                                if(mem[buffer2.front()] == 'W')
+                                {
+                                    diskWrites++;
+                                }
+                                mem.erase(buffer2.front());
+                                buffer2.erase(buffer2.begin());
+                                //push replaced buffer1 frame into the back of LRU table (buffer2)
+                                buffer2.push_back(tempFrame);
+                                mem[tempFrame] = tempRW;
+                            }
+                            break;
+
+                        case 2: //frame is in LRU table
                             if(strcmp(debugOrQuiet, "debug") == 0){
-                                std::cout<< count <<": Buffer2 is full with no matches, adding frame to buffer1 and shifting buffer1 replacement to buffer2" << std::endl;
+                                std::cout<< count <<": Buffer2 entry is a match, moving this entry to buffer1" << std::endl;
                             }
                             int replacementIndex = buffer1.calculateCircularIndex();
                             unsigned tempFrame = buffer1.array[replacementIndex].first;
                             char tempRW = buffer1.array[replacementIndex].second;
-
-                            buffer1.array[replacementIndex].first = frameNum;
-                            buffer1.array[replacementIndex].second = rw;
-                            buffer1.incrementLoopOffset();
                             
-                            if(mem[buffer2.front()] == 'W')
+                            if((mem[frameNum] != 'W') && (rw == 'W'))   
                             {
-                                diskWrites++;
+                                mem[frameNum] = rw;
                             }
-                            mem.erase(buffer2.front());
-                            buffer2.erase(buffer2.begin());
+                            buffer1.array[replacementIndex].first = frameNum;
+                            buffer1.array[replacementIndex].second = mem[frameNum];
+                            buffer1.incrementLoopOffset();
+                            buffer2.erase(It);
+                            mem.erase(frameNum);
                             //push replaced buffer1 frame into the back of LRU table (buffer2)
                             buffer2.push_back(tempFrame);
                             mem[tempFrame] = tempRW;
-                        }
-                        break;
-
-                    case 2: //frame is in LRU table
-                        if(strcmp(debugOrQuiet, "debug") == 0){
-                            std::cout<< count <<": Buffer2 entry is a match, moving this entry to buffer1" << std::endl;
-                        }
-                        int replacementIndex = buffer1.calculateCircularIndex();
-                        unsigned tempFrame = buffer1.array[replacementIndex].first;
-                        char tempRW = buffer1.array[replacementIndex].second;
-                        
-                        if((mem[frameNum] != 'W') && (rw == 'W'))   
-                        {
-                            mem[frameNum] = rw;
-                        }
-                        buffer1.array[replacementIndex].first = frameNum;
-                        buffer1.array[replacementIndex].second = mem[frameNum];
-                        buffer1.incrementLoopOffset();
-                        buffer2.erase(It);
-                        mem.erase(frameNum);
-                        //push replaced buffer1 frame into the back of LRU table (buffer2)
-                        buffer2.push_back(tempFrame);
-                        mem[tempFrame] = tempRW;
-                        break;
+                            break;
+                    }
+                    break;
                 }
-                break;
             }
         }
     }
-
 
     std::cout << "Total memory frames: " << nframes << std::endl;
     std::cout << "Events in trace: " << count << std::endl;
